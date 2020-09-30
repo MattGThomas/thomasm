0a2567be6644a15e280efa5ff933ad27357f46b1
MattGThomas <mgthomas0616@gmail.com>
2020-09-30 09:13:51 -0500

    changed config file to test on server

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,12 +1,12 @@
-// module.exports = {
-//   HOST: "52.86.154.61",
-//   USER: "thomasm",
-//   PASSWORD: "cWazFrcuadLjFDna",
-//   DB: "thomasm",
-// };
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "Olivia0721",
+  HOST: "52.86.154.61",
+  USER: "thomasm",
+  PASSWORD: "cWazFrcuadLjFDna",
   DB: "thomasm",
 };
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "Olivia0721",
+//   DB: "thomasm",
+// };
