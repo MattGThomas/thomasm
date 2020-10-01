271409b8515f0945afefc1f9d7f897c692489a92
abdul basith siddiqui <mohdabdulbasith17@gmail.com>
2020-10-01 15:50:49 +0530

    Integer value between two numbers

---
+++ getrandomnumber.js
@@ -0,0 +1,9 @@
+// input from the user
+let min = parseInt(prompt("Enter a min value: "));
+let max = parseInt(prompt("Enter a max value: "));
+
+// generating a random number
+let a = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// display a random number
+console.log(`Random value between ${min} and ${max} is ${a}`);
